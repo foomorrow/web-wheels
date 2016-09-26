f05e66844de5c782af26fe34ebfa9f59ef3075af
foomorrow <stormsong3@gmail.com>
2016-09-26 08:16:22 +0800

    vd init

---
+++ src/virtualdom/vdom.js
@@ -0,0 +1,41 @@
+function VNode(tag, props, children){
+  this.tag = tag
+  this.props = props
+  this.children = children
+  const { key } = props
+}
+function Component(props, context){
+
+}
+function createElement(tag, props, ...children){
+  if(typeof tag === 'function'){
+    return new tag(Object.assign({}, props, { children }), context)
+  }else{
+    return new VNode(tag, props, children)
+  }
+}
+Object.assign(VComponent.prototype, {
+  setState(){
+
+  }
+  replaceState(){
+
+  }
+  forceUpdate(){
+
+  }
+  componentWillMount(){
+
+  }
+  componentDidMount(){
+
+  }
+  render(){
+    throw new Error('must impl')
+  }
+})
+export {
+  VNode,
+  VComponent,
+  createElement
+}
